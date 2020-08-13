543493ae028e1f3e984d04d8bf223dbaf7f93df2
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-13 09:45:06 +0200

    Create SettingsActivity

--- app/src/main/java/com/example/uprightchallenge/SettingsActivity.java
+++ app/src/main/java/com/example/uprightchallenge/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.uprightchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 }
