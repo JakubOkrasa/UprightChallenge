b5275040e49dcb24e2b1bf2032021ce59c2952cd
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-13 09:35:22 +0200

    add preferences.xml

---
+++ app/src/main/java/com/example/uprightchallenge/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.uprightchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/uprightchallenge/SettingsFragment.java
@@ -0,0 +1,31 @@
+package com.example.uprightchallenge;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+}
