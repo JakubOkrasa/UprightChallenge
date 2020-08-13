271e8f879c354490f2a2aae33da3cb3837f1fe62
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-13 09:36:20 +0200

    Create SettingsFragment.java

--- app/src/main/java/com/example/uprightchallenge/SettingsFragment.java
+++ app/src/main/java/com/example/uprightchallenge/SettingsFragment.java
@@ -12,20 +12,11 @@ import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
-public class SettingsFragment extends Fragment {
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
