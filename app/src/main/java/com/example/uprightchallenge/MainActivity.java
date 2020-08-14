0cd7a5912edc66f16e16dfd07cf22e725a51c3b3
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-14 15:40:20 +0200

    setOnChangePreferenceListener for notif. switch

--- app/src/main/java/com/example/uprightchallenge/MainActivity.java
+++ app/src/main/java/com/example/uprightchallenge/MainActivity.java
@@ -24,14 +24,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 // todo #bug notif toggle is on at the beginning (sometimes?)
 // todo #later show number of daily count in notification
+// todo show info in MainActivity when notification are turned off
 
 // todo #note when I added if(savedInstanceState != null) {..} and onRestore lines, AND in the app click BACK button to check the right count number, nothing happens
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final String CORRECT_POSTURE_COUNT_KEY = "count";
-    private NotificationManager mNotifyManager;
-    private static final int NOTIFICATION_ID = 0;
-    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private CorrectPostureReceiver mCorrectPostureReceiver = new CorrectPostureReceiver(this);
     static final String POSTURE_YES_ACTION = BuildConfig.APPLICATION_ID + ".POSTURE_YES_ACTION";
 
@@ -41,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createNotificationChannel();
 
         //ToggleButton mNotifToggle = findViewById(R.id.notify_toggle);
         TextView mCorrectPostureTextView = findViewById(R.id.txt_count);
@@ -49,44 +46,11 @@ public class MainActivity extends AppCompatActivity {
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //set alarmPendingIntent to deliver repeating notifications
-        final AlarmManager mAlarmManager = (AlarmManager) this.getSystemService(ALARM_SERVICE);
-        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
-        //mNotifToggle.setChecked(PendingIntent.getBroadcast(this, NOTIFICATION_ID, alarmIntent, PendingIntent.FLAG_NO_CREATE) != null); // check if notifications were turned on before the new MainActivity was stopped
-        final PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
         registerReceiver(mCorrectPostureReceiver, new IntentFilter(POSTURE_YES_ACTION));
 
         String sharedPrefsFile = BuildConfig.APPLICATION_ID;
         SharedPreferences preferences = getSharedPreferences(sharedPrefsFile, MODE_PRIVATE);
-        /*
-        mNotifToggle.setOnCheckedChangeListener(
-                 new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        String toastMessage = "error: AlarmManager is null";
-                        if(isChecked) {
-                            long repeatInterval;
-                            if(Build.FINGERPRINT.startsWith("google/sdk_gphone_x86/generic")) { repeatInterval = 30000; }//short interval only for debug
-                            else { repeatInterval = AlarmManager.INTERVAL_HALF_HOUR; }
 
-                            long triggerTime = SystemClock.elapsedRealtime() + repeatInterval;
-                            if(mAlarmManager!=null) {
-                                mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, repeatInterval, alarmPendingIntent);
-                                toastMessage = "Upright notifications on.";
-                            }
-                        }
-                        else {
-                            mNotifyManager.cancelAll();
-                            if (mAlarmManager!=null) {
-                                mAlarmManager.cancel(alarmPendingIntent);
-                            }
-                            toastMessage = "Upright notifications off.";
-                        }
-                        Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
-                    }
-                }
-        ); */
 
         if(savedInstanceState != null) {
             int count = savedInstanceState.getInt(CORRECT_POSTURE_COUNT_KEY);
@@ -160,20 +124,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private void createNotificationChannel() {
-        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        long[] vibPattern = {0, 200, 200, 200, 200, 200}; //{delay1, vibDuration1, delay2, vibDuration2...}
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(
-                    PRIMARY_CHANNEL_ID,
-                    "Check posture notification",
-                    NotificationManager.IMPORTANCE_HIGH);
-            notificationChannel.setDescription("notification check posture");
-            notificationChannel.enableVibration(true);
-            notificationChannel.setVibrationPattern(vibPattern);
-            mNotifyManager.createNotificationChannel(notificationChannel);
-        }
-    }
+
 
 
 }
--- app/src/main/java/com/example/uprightchallenge/SettingsFragment.java
+++ app/src/main/java/com/example/uprightchallenge/SettingsFragment.java
@@ -1,22 +1,88 @@
 package com.example.uprightchallenge;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.Context.ALARM_SERVICE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends PreferenceFragmentCompat {
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        //set alarmPendingIntent to deliver repeating notifications
+        final AlarmManager mAlarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
+        Intent alarmIntent = new Intent(getContext(), AlarmReceiver.class);
+        final PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(getContext(), NOTIFICATION_ID, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        createNotificationChannel();
+        SwitchPreferenceCompat notificationSwitch = findPreference("switch_notifications");
+
+        if(notificationSwitch!=null) {
+            notificationSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object notificationsOnObject) {
+                    boolean notificationsOn = (Boolean) notificationsOnObject;
+                    if(notificationsOn) {
+                        long repeatInterval;
+                        if (Build.FINGERPRINT.startsWith("google/sdk_gphone_x86/generic")) { repeatInterval = 30000; }//short interval only for debug
+                        else { repeatInterval = AlarmManager.INTERVAL_HALF_HOUR; }
+
+                        long triggerTime = SystemClock.elapsedRealtime() + repeatInterval;
+                        if (mAlarmManager != null) {
+                            mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, repeatInterval, alarmPendingIntent);
+                        }
+                    }
+                    else {
+                        mNotifyManager.cancelAll();
+                        if (mAlarmManager!=null) {
+                            mAlarmManager.cancel(alarmPendingIntent);
+                        }
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void createNotificationChannel() {
+        mNotifyManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        long[] vibPattern = {0, 200, 200, 200, 200, 200}; //{delay1, vibDuration1, delay2, vibDuration2...}
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID,
+                    "Check posture notification",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("notification check posture");
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(vibPattern);
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
