ac0ffe62411da0aeda4c4c77171fdb0ce1b1f277
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-18 10:41:58 +0200

    remove usage of MainActivity in CorrectPostureReceiver

--- app/src/main/java/com/example/uprightchallenge/CorrectPostureReceiver.java
+++ app/src/main/java/com/example/uprightchallenge/CorrectPostureReceiver.java
@@ -14,32 +14,18 @@ class CorrectPostureReceiver extends BroadcastReceiver {
     private int mCount = 0;
     private TextView mTxtCount;
     private final String LOG_TAG = CorrectPostureReceiver.class.getSimpleName();
-    private MainActivity mainActivity;
     private static final String KEY_YES_POSTURE_COUNT = "yes_posture_count";
 
-    public CorrectPostureReceiver(MainActivity mainActivity) {
-        this.mainActivity = mainActivity;
+    public CorrectPostureReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(LOG_TAG, "yes option clicked");
-        mTxtCount = mainActivity.findViewById(R.id.txt_count); //todo mCount initialize from prefs, mayby mainActivity not necessary
-        if (mTxtCount != null) {
-            mCount++;
-//            mTxtCount.setText(Integer.toString(mCount));
-        }
-
+        mCount++;
         final String sharedPrefFile = BuildConfig.APPLICATION_ID;
         SharedPreferences preferences =  context.getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
         SharedPreferences.Editor prefEditor = preferences.edit();
         prefEditor.putInt(KEY_YES_POSTURE_COUNT, mCount).apply();
-    }
-
-    public int getCount() {
-        return mCount;
-    }
-    public TextView getTxtCount() {
-        return mTxtCount;
     }
 }
