b01fee765c5977d29350d7be86630067d642f68a
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-22 08:55:30 +0200

    handle bad posture action button in AlarmReceiver

--- app/src/main/java/com/example/uprightchallenge/AlarmReceiver.java
+++ app/src/main/java/com/example/uprightchallenge/AlarmReceiver.java
@@ -5,13 +5,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
-import android.widget.TextView;
 
 import androidx.core.app.NotificationCompat;
-
-import static android.content.Context.MODE_PRIVATE;
 
 //receive periodically notifications pending intents
 public class AlarmReceiver extends BroadcastReceiver {
@@ -43,12 +39,16 @@ public class AlarmReceiver extends BroadcastReceiver {
         Intent postureYesIntent = new Intent(MainActivity.POSTURE_YES_ACTION);
         PendingIntent yesPendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_ID, postureYesIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        Intent postureNoIntent = new Intent(MainActivity.POSTURE_NO_ACTION);
+        PendingIntent noPendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_ID, postureNoIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
                 .setContentTitle("Are you straighten up?")
                 .setSmallIcon(R.drawable.ic_notify)
                 .setAutoCancel(true) // closes the notification when user taps on it
                 .setContentIntent(notifClickPendingIntent)
                 .addAction(R.drawable.ic_posture_yes, "yes", yesPendingIntent)
+                .addAction(R.drawable.ic_posture_no, "no", noPendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
         mNotificationManager.notify(NOTIFICATION_ID, builder.build());
