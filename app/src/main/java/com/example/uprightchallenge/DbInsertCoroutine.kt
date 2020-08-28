3f0b0d4c50774b3181f6f296e4eb8bcff043a621
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-08-28 10:00:40 +0200

    dbInsertCoroutine (Kotlin)

--- app/src/main/java/com/example/uprightchallenge/DbInsertCoroutine.kt
+++ app/src/main/java/com/example/uprightchallenge/DbInsertCoroutine.kt
@@ -1,4 +1,15 @@
 package com.example.uprightchallenge
 
-class DbInsertCoroutine {
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+abstract class DbInsertCoroutine<Params, Progress, Result> {
+    abstract fun doInBackground(params: Params): Result
+
+    fun execute(vararg params: Params) {
+        GlobalScope.launch(Dispatchers.Default) { // TODO: 8/28/2020 check coroutineScope
+            doInBackground(params[0])
+        }
+    }
 }
