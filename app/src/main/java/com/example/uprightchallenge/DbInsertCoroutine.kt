0cf0ffa1862f1befa93d409a12cfae1ace6af32c
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-09-03 10:21:36 +0200

    DbInsertCoroutine - CoroutineScope and:
    *remove unused params

--- app/src/main/java/com/example/uprightchallenge/DbInsertCoroutine.kt
+++ app/src/main/java/com/example/uprightchallenge/DbInsertCoroutine.kt
@@ -1,15 +1,16 @@
 package com.example.uprightchallenge
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.example.uprightchallenge.data.PostureStat
+import com.example.uprightchallenge.data.PostureStatDao
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 
-abstract class DbInsertCoroutine<Params, Progress, Result> {
-    abstract fun doInBackground(params: Params): Result
+abstract class DbInsertCoroutine {
 
-    fun execute(vararg params: Params) {
-        GlobalScope.launch(Dispatchers.Default) { // TODO: 8/28/2020 check coroutineScope
-            doInBackground(params[0])
+    open fun insertStat(dao: PostureStatDao, stat: PostureStat) {
+        CoroutineScope(IO).launch  {
+            dao.insert(stat)
         }
     }
+
 }
--- app/src/main/java/com/example/uprightchallenge/DbSelectCoroutine.kt
+++ app/src/main/java/com/example/uprightchallenge/DbSelectCoroutine.kt
@@ -1,6 +1,11 @@
 package com.example.uprightchallenge
 
+import com.example.uprightchallenge.data.PostureStat
+import com.example.uprightchallenge.data.PostureStatDao
 import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.future.future
+import java.util.concurrent.CompletableFuture
 
 abstract class DbSelectCoroutine<Params, Progress, Result> {
     abstract fun doInBackground(): Result
