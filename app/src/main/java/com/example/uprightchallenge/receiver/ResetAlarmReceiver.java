0d1224dd50660d9dec6faf954506798197b84472
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-09-05 13:10:04 +0200

    add a log and a comment

--- app/src/main/java/com/example/uprightchallenge/receiver/ResetAlarmReceiver.java
+++ app/src/main/java/com/example/uprightchallenge/receiver/ResetAlarmReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.example.uprightchallenge.BuildConfig;
 import com.example.uprightchallenge.data.PostureStat;
@@ -14,12 +15,14 @@ public class ResetAlarmReceiver extends BroadcastReceiver {
     public static final String sharedPrefsFile = BuildConfig.APPLICATION_ID;
     private static final String PREF_KEY_GOOD_POSTURE_COUNT = "good_posture_count";
     private static final String PREF_KEY_BAD_POSTURE_COUNT = "bad_posture_count";
+    public static final String LOG_TAG = ResetAlarmReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         SharedPreferences preferences = context.getSharedPreferences(sharedPrefsFile, Context.MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = preferences.edit();
 
+        // save stats to repository
         mRepository=PostureStatRepository.getRepository(context);
         mRepository.insert(new PostureStat(
                 0,
@@ -31,5 +34,6 @@ public class ResetAlarmReceiver extends BroadcastReceiver {
         prefsEditor.putInt(PREF_KEY_GOOD_POSTURE_COUNT, 0);
         prefsEditor.apply();
 
+        Log.e(LOG_TAG, "new PostureStat saved");
     }
 }
