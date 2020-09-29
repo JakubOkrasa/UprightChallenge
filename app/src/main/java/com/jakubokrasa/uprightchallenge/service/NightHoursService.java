c24bdbbec0707c70d00023dd7e7720e1e73864f9
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-09-29 16:31:04 +0200

    create NightHoursService which starts when is destroyed

---
+++ app/src/main/java/com/jakubokrasa/uprightchallenge/service/NightHoursService.java
@@ -0,0 +1,21 @@
+package com.jakubokrasa.uprightchallenge.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class NightHoursService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
