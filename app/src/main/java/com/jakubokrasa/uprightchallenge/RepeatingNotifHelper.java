a6a8c5b4744277df465916913d5a7a15ded40fc6
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-10-13 09:20:33 +0200

    refactor and improme setNotifOnTimeRange method
    change setCalendar to getCalendar
    handle case if end time is ealier than begin time

--- app/src/main/java/com/jakubokrasa/uprightchallenge/RepeatingNotifHelper.java
+++ app/src/main/java/com/jakubokrasa/uprightchallenge/RepeatingNotifHelper.java
@@ -78,35 +78,25 @@ public class RepeatingNotifHelper {
         PendingIntent notifOnTimePendingIntent = PendingIntent.getBroadcast(context, NOTIF_ON_TIME_ALARM, notifOnTimeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         PendingIntent notifOffTimePendingIntent = PendingIntent.getBroadcast(context, NOTIF_ON_TIME_ALARM, notifOffTimeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Calendar calendar = Calendar.getInstance();
-
-        //set turn on notifications time
-        calendar.setTimeInMillis(System.currentTimeMillis());
         String notifOnTime = preferences.getString(context.getResources().getString(R.string.pref_key_notif_on_time), "7:30");
         String notifOffTime = preferences.getString(context.getResources().getString(R.string.pref_key_notif_off_time), "21:00");
-        // TODO: 10/12/2020 check if notifOnTime<notifOffTime, otherwise day+1
-        // TODO: 10/12/2020 what if
-        //  1. start nofif set at 10 
-        //  2. end notif set at 7 (+1day)
-        //  3. start notif set at 5 and now is 3.
-        //  >> end notif will be on the next day
+
+        Calendar notifOnCal = getCalendar(notifOnTime);
+        Calendar notifOffCal = getCalendar(notifOffTime);
+        if(notifOnCal.getTimeInMillis() > notifOffCal.getTimeInMillis()) { notifOffCal.add(Calendar.DATE, 1); }
+        mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, notifOnCal.getTimeInMillis(), AlarmManager.INTERVAL_DAY, notifOnTimePendingIntent);
+        mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, notifOffCal.getTimeInMillis(), AlarmManager.INTERVAL_DAY, notifOffTimePendingIntent);
         Log.d(LOG_TAG, "notif begin time: " + notifOnTime);
         Log.d(LOG_TAG, "notif end time: " + notifOffTime);
-        setCalendar(calendar, notifOnTime);
-        mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, notifOnTimePendingIntent);
-
-        //set turn off notifications time
-        setCalendar(calendar, notifOffTime);
-
-//            calendar.add(Calendar.DATE, 1); //uncomment if not debug todo handle cases when DATE + 1 is unwanted
-        mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, notifOffTimePendingIntent);
-
     }
 
-    private void setCalendar(Calendar calendar, String timePref) {
+    private Calendar getCalendar(String timePref) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
         String[] timeArr = timePref.split(":");
         calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArr[0]));
         calendar.set(Calendar.MINUTE, Integer.parseInt(timeArr[1]));
+        return calendar;
     }
 
 }
